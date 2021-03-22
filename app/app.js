f1fbb79f24f96bb4541c3dd3d15475df21e3e43c
Vjekoni <dizdar.vjeko@gmail.com>
2021-03-22 20:48:19 +0100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
 //variables
 const form = document.querySelector("#form");
-const inputBorder = document.querySelector("#email");
+const input = document.querySelector("#email");
 const small = document.querySelector(".js-show");
+const btn = document.querySelector("#submit-btn")
 
 // event listener for submit, preventing default behaviour
 form.addEventListener('submit', (e) => {
@@ -21,10 +22,12 @@ function submit() {
       const change = document.querySelector(".submit-change");
       let mail = document.querySelector("#email").value;
       change.innerHTML = mail + " signed up for weekly newsletter. We will send You notification on email!";
+      btn.style.display = "none";
+      input.stlye.display = "none;"
 }
 
 function error() {
-      inputBorder.classList.add("error")
+      input.classList.add("error")
       small.classList.add("error")
 }
 
@@ -38,7 +41,7 @@ function email() {
                   small.innerHTML = "Oops looks like " + mail + " is not a valid email"
             }
             else {
-                  inputBorder.classList.remove("error")
+                  input.classList.remove("error")
                   small.classList.remove("error")
                   submit()
             }
